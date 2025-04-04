b5372d0a834ef15f78071b8e4bfe2048a51b75c8
ccarissimo <N20kztu!>
2025-04-04 17:06:18 +0200

    edits to text

--- article.py
+++ article.py
@@ -1,23 +1,38 @@
 import streamlit as st
 
 # Page Title
-st.title("Algorithmic Collusion by Algorithm Orchestration")
+st.title("AI and Collusion")
 
 # Placeholder for Markdown content
 article_content = """
-Do algorithms make collusion easier? In short, yes.
-In so far as automation makes things easier, algorithms also make collusion easier.
-But making collusion easier with algorithms requires collusion between algorithm designers.
-This is the main point we make in our latest work.
 
-Algorithms can automate collusion for us. Does this mean we should ban all algorithms?
-No. Companies can establish price collusion, but we do not ban companies.
-Because, just as we can use algorithms to automate collusion, we can also use them to automate competition.
+The rise of AI is linked to the phenomenon of algorithmic collusion. We may wonder:
+
+- Do algorithms make collusion easier?
+- Should we ban pricing algorithms?  
+- What can be done to check, fix, etc.?
+
+Short answers: yes, yes, a lot.
+
+Now with a bit more detail:
+
+### Do algorithms make collusion easier? 
+
+Yes, they do in so far as automation makes anything easier to implement, algorithms also make collusion easier. But making collusion easier with algorithms requires collusion between algorithm designers. This is the main point we make in our latest work.
+
+Algorithms can automate collusion for us. 
+
+### Does this mean we should ban all algorithms?
+
+No. Companies can establish price collusion, but we do not ban companies. Because, just as we can use algorithms to automate collusion, we can also use them to automate competition.
+
+### What can be done to regulate algorithmic collusion?
 
 We must allow for algorithms that are *designed competitively*, and lead to competitive pricing. On the other hand, we must identify algorithms that are **orchestrated**, and lead to collusive pricing. 
 
-We distinguish competitively designed algorithms from orchestrated algorithms by treating the algorithms as strategies, and determining whether or not the picked algorithm is a selfish profit maximising best response to the market.
+### What do we propose?
 
+We distinguish competitively designed algorithms from orchestrated algorithms by treating the algorithms as strategies, and determining whether or not the picked algorithm is a selfish profit maximising best response to the market.
 """
 
 # Display Markdown content

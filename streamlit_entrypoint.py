5c55b8fa65611b360f0036e968f36b8e52a09e02
ccarissimo <N20kztu!>
2025-06-10 14:40:58 +0200

    add videos

--- streamlit_entrypoint.py
+++ streamlit_entrypoint.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-pg = st.navigation([st.Page("overview.py"), st.Page("hotel_pricing.py")])
+pg = st.navigation([st.Page("overview.py"), st.Page("hotel_pricing.py"), st.Page("videos.py")])
 pg.run()
---
+++ videos.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+VIDEO_URL = "https://www.youtube.com/watch?v=McXM1380Ii4&t=3s"
+st.video(VIDEO_URL)

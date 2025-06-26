4faa3917d9e2c733ec4b371b09c0a402c5758d74
ccarissimo <N20kztu!>
2025-06-26 15:55:33 +0200

    overview image

--- overview.py
+++ overview.py
@@ -35,5 +35,8 @@ We must allow for algorithms that are *designed competitively*, and lead to comp
 We distinguish competitively designed algorithms from orchestrated algorithms by treating the algorithms as strategies, and determining whether or not the picked algorithm is a selfish profit maximising best response to the market.
 """
 
+# Display image
+st.image("images/age_of_ai.png", caption="Algorithms change competition and collusion.")
+
 # Display Markdown content
 st.markdown(article_content)
